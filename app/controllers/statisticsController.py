b05f021e09e58fc7d9c873f84f5c821362e35e2c
Deuana <deuana.telles@hotmail.com>
2018-09-11 18:44:28 -0300

    Auto stash before merge of "devel" and "origin/devel"

---
+++ app/controllers/statisticsController.py
@@ -0,0 +1,27 @@
+from flask import render_template, redirect, request, url_for
+import shlex
+import subprocess
+from app import app
+
+
+
+@app.route("/statistics", methods=["GET", "POST"])
+def statistics():
+
+    command = "C:/Users/Deuana/Documents/R/R-3.5.1/bin/Rscript C:/Users/Deuana/Desktop/Project/statistics_political_parties.R"
+    args = shlex.split(command)
+
+    try:
+        p = subprocess.run(args, check=True)
+        if p.returncode == 0:
+            msg = "Coleta realizada com sucesso."
+            return render_template("index.html", msg=msg)
+        else:
+            error = "Não foi possível realizar a consulta."
+            return render_template("index.html", error=error)  
+
+    except subprocess.CalledProcessError as error:
+        error = "Não foi possível realizar a consulta."
+        return render_template("index.html", error=error)  
+
+    return render_template("index.html")
--- app/controllers/streamController.py
+++ app/controllers/streamController.py
@@ -7,19 +7,33 @@ from app.models.streamForm import streamForm
 @app.route("/stream", methods=["GET", "POST"])
 def stream():
     form = streamForm()
+    
     if form.validate_on_submit():
+        
         trackKeyword = form.trackKeyword.data
         trackTimeout = form.trackTimeout.data
         dbCollection = form.dbCollection.data
-        #print(trackKeyword, trackTimeout, dbCollection)
-        command= "C:/Users/Deuana/Documents/R/R-3.5.1/bin/Rscript C:/Users/Deuana/Desktop/Project/tweet_stream_rtweet.R"
-        args = shlex.split(command)
-        args.append(trackKeyword)
-        args.append(trackTimeout)
-        args.append(dbCollection)
-        p = subprocess.Popen(args)
-    else:
-        error= "Dados inválidos. Insira novamente por favor."
-        return render_template("streamForm.html", form= form, error= error)
+        
+        if trackKeyword and trackTimeout and dbCollection: 
+            #print(trackKeyword, trackTimeout, dbCollection)
+            command= "C:/Users/Deuana/Documents/R/R-3.5.1/bin/Rscript C:/Users/Deuana/Desktop/Project/tweet_stream_rtweet.R"
+            args = shlex.split(command)
+            args.append(trackKeyword)
+            args.append(trackTimeout)
+            args.append(dbCollection)
+            
+            try:
+                p = subprocess.run(args, check=True)
+                if p.returncode == 0:
+                    msg= "Coleta realizada com sucesso."
+                    return render_template("streamForm.html", form= form, msg= msg)
+
+            except subprocess.CalledProcessError as error:
+               error= "Não foi possível realizar a consulta."
+               return render_template("streamForm.html", form= form, error= error)
+             
+        else:
+            error= "Dados inválidos. Insira novamente por favor."
+            return render_template("streamForm.html", form= form, error= error)
 
     return render_template("streamForm.html", form= form)
