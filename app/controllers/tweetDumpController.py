85735dda2c9920dd876e9be120eb25de22c379d1
Deuana <deuana.telles@hotmail.com>
2018-09-12 18:09:51 -0300

    Auto stash before merge of "devel" and "origin/devel"

--- app/controllers/tweetDumpController.py
+++ app/controllers/tweetDumpController.py
@@ -4,15 +4,14 @@ import subprocess
 from app import app
 
 
+@app.route("/tweetdump", methods=["GET", "POST"])
+def tweetdump():
 
-@app.route("/getFriends", methods=["GET", "POST"])
-def getFriends():
-
-    command = "C:/Users/Deuana/Documents/R/R-3.5.1/bin/Rscript C:/Users/Deuana/Desktop/Project/get_friends_rtweet.R"
+    command = "C:/Users/Deuana/Documents/R/R-3.5.1/bin/Rscript C:/Users/Deuana/Desktop/Project/candidates_timelines.R"
     args = shlex.split(command)
 
     try:
-        p = subprocess.run(args, check=True, shell = True)
+        p = subprocess.run(args, check=True)
         if p.returncode == 0:
             msg = "Coleta realizada com sucesso."
             return render_template("index.html", msg=msg)
